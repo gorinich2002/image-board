d5df2a1780b0bf11441326e4e5e0f81cbc610a96
gorinich2002 <egormaysnikov@gmail.com>
2022-08-16 00:00:24 +0300

    fix pg docker and add squileze module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
@@ -7,9 +8,20 @@ import { AppService } from './app.service';
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env'
+    }),
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: 'localhost',
+      port : 5432,
+      username: 'root',
+      password: 'root',
+      database: 'postgres',
+      models: [],
+      autoLoadModels:true
+
     })
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
